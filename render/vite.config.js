304e3ce159692fc2d3e5bc5a5b7ad1d7c6fd8536
utemix-lab <moralitedmitry@gmail.com>
2026-02-03 13:36:03 +0300

    Copy dream-graph approach - simple base path and simple deployment

--- render/vite.config.js
+++ render/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: '/',
+  base: "/vovaipetrova/",
   build: {
     target: "esnext",
     rollupOptions: {
